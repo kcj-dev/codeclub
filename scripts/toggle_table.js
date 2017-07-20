2b4db6795fa67aed976b919eeea5d9ea871b31e8
kimhyonh <pengkim.professional@gmail.com>
2017-07-20 15:43:13 -0400

    finished curriculum html and javascript

---
+++ scripts/toggle_table.js
@@ -0,0 +1,45 @@
+
+$('document').ready(function(){
+  var $scratch1 = $('#scratch1Btn');
+  var $scratch2 = $('#scratch2Btn');
+  var $webdev1Btn = $('#webdev1Btn');
+  var $webdev2Btn = $('#webdev2Btn');
+  var $python1Btn = $('#python1Btn');
+  var $python2Btn = $('#python2Btn');
+
+  $scratch1.click(addActive);
+  $scratch1.click(function(){
+    $scratch2.removeClass('active');
+  });
+  $scratch2.click(addActive);
+
+  $webdev1Btn.click(addActive);
+  $webdev1Btn.click(function(){
+    $webdev2Btn.removeClass('active');
+  });
+  $webdev2Btn.click(addActive);
+
+  $python1Btn.click(addActive);
+  $python1Btn.click(function(){
+    $python2Btn.removeClass('active');
+  });
+  $python2Btn.click(addActive);
+});
+
+function addActive(e){
+  //e.preventDefault();
+//  removeActive();
+  var $btn = $(this);
+  $btn.tab('show')
+  $btn.addClass('active');
+//  console.log($btn.parent().children().length);
+}
+
+function removeActive($btn){
+  var btns = $btn.parent().children();
+  btns.removeClass('active');
+//  console.log(btns);
+  /*for(var i=0; i<btns.length; i++){
+    btns[i]
+  }*/
+}

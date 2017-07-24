9865686b427f132790a639992d75191a09082c7a
thatNinja42 <josepepin2@gmail.com>
2017-07-24 15:11:29 -0700

    faq changes

---
+++ scripts/faq_custom.js
@@ -0,0 +1,23 @@
+var g = {};
+$(document).ready(function() {
+
+
+g.titles = document.getElementsByClassName("panel-title");
+$(g.titles).click(function(e) {
+  var target = e.target || e.srcElement;
+  target = target.parentElement.parentElement;
+
+  if(target.className == "panel-title glyphicon glyphicon-triangle-bottom") {
+    for (var i = 0; i < g.titles.length; i++) {
+      g.titles[i].className = "panel-title glyphicon glyphicon-triangle-bottom";
+    }
+    target.className = "panel-title glyphicon glyphicon-triangle-top";
+  } else if (target.className == "panel-title glyphicon glyphicon-triangle-top") {
+    for (var i = 0; i < g.titles.length; i++) {
+      g.titles[i].className = "panel-title glyphicon glyphicon-triangle-bottom";
+    }
+    target.className = "panel-title glyphicon glyphicon-triangle-bottom";
+  }
+});
+
+});

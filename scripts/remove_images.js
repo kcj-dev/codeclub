72429492241ef54f06b844c3fa1cdad3eebae241
thatNinja42 <josepepin2@gmail.com>
2017-07-26 15:19:17 -0700

    resizing script patch

--- scripts/remove_images.js
+++ scripts/remove_images.js
@@ -2,14 +2,17 @@ var g = {};
 $(document).ready(function() {
 
   g.img = document.getElementsByClassName("after-nav")[0].getElementsByTagName("img");
-
-  $( window ).resize(function() {
-    if($(window).width() < 850 || $(document).width() < 1000){
-      $(g.img[0]).hide();
-      $(g.img[1]).hide();
-    } else if ($(window).width() > 850 || $(document).width() > 1000) {
-      $(g.img[0]).show();
-      $(g.img[1]).show();
-    }
+  checkwindowSize();
+  $(window).resize(function(){
+    checkwindowSize();
   });
 });
+function checkwindowSize (){
+  if($(window).width() < 850 || $(document).width() < 1000){
+    $(g.img[0]).hide();
+    $(g.img[1]).hide();
+  } else if ($(window).width() > 850 || $(document).width() > 1000) {
+    $(g.img[0]).show();
+    $(g.img[1]).show();
+  }
+}
